678990228cf17d9e896a5d88f537d7ba955346f1
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-08-27 11:08:48 -0300

    refactor: remove website footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,15 +14,6 @@ export default function IndexRoute() {
       <div className="container mx-auto flex px-5 xl:py-48 py-40 md:flex-row flex-col items-center justify-center">
         <Profile />
       </div>
-
-      <footer className="text-xs text-neutral-400">
-        <div className="text-center px-5 xl:py-1 py-8 container mx-auto items-center-justify-center sm:flex-row flex-col">
-          Made with ❤ by{' '}
-          <a className="underline underline-offset-2 hover:text-blue-500 transition-all duration-300 delay-100" href="https://github.com/whoisniskii">
-            Nicolas Ribeiro
-          </a>
-        </div>
-      </footer>
     </main>
   );
 }
