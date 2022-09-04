5eac11a5e02c4cccaf4e4b7f531edcd34bfd432e
Nicolas Ribeiro <lunary@niskii.dev>
2022-09-04 19:57:27 -0300

    add miss tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Profile from '../components/discord/Profile';
 
 export default function IndexRoute() {
   return (
-    <main className="flex mx-auto items-center justify-center h-screen">
+    <main className="flex mx-auto items-center justify-center h-screen w-screen">
       <Head>
         <meta charSet="UTF-8" />
         <link rel="canonical" href="https://niskii.dev" />
