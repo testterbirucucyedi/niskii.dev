178b68e94592d4c2a4f253f75b4d1a4d55cba4ce
Nicolas Ribeiro <lunary@niskii.dev>
2022-09-04 21:26:21 -0300

    fix issues

--- src/components/discord/icons/AnchorIcon.tsx
+++ src/components/discord/icons/AnchorIcon.tsx
@@ -1,7 +1,7 @@
 export default function LinkIcon() {
   return (
     <svg aria-hidden="true" role="img" width="24" height="24" className="rotate-45" viewBox="0 0 24 24">
-      <polygon fill="currentColor" fill-rule="nonzero" points="13 20 11 20 11 8 5.5 13.5 4.08 12.08 12 4.16 19.92 12.08 18.5 13.5 13 8" />
+      <polygon fill="currentColor" fillRule="nonzero" points="13 20 11 20 11 8 5.5 13.5 4.08 12.08 12 4.16 19.92 12.08 18.5 13.5 13 8" />
     </svg>
   );
 }
