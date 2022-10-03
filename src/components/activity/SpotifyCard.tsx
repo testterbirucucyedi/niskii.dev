63f532ae1f8ed455e0a54772fd3c8f0cc4992f4c
sel <70216275+selim1337@users.noreply.github.com>
2022-10-03 16:27:44 +0200

    update spotify image

--- src/components/activity/SpotifyCard.tsx
+++ src/components/activity/SpotifyCard.tsx
@@ -23,7 +23,7 @@ export function SpotifyCard({ spotify }: CardProps) {
       {/* Image */}
       <div className="items-center flex flex-auto">
         <div className="relative self-start">
-          <img src={spotify.album_art_url} alt={spotify.album} width="60" height="60" className="block object-cover" />
+          <img src={spotify.album_art_url} alt={spotify.album} width="60" height="60" className="block object-cover rounded-lg" />
         </div>
 
         <div className="flex-auto overflow-hidden ml-2.5 text-normal">
