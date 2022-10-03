2f0464476bad7c1c7259878d86b5553efa10403c
sel <70216275+selim1337@users.noreply.github.com>
2022-10-03 16:17:46 +0200

    update about

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -3,10 +3,10 @@ export function AboutCard() {
     <div className="mb-3">
       <h2 className="font-bold text-xs text-slate-300 leading-4 mb-2 uppercase">About Me</h2>
       <div className="whitespace-pre-line text-normal">
-        {'he/they, brazilian\n\n'}
+        {'beni kaybedersen bulamazsin\n\n'}
 
-        <a href="https://niskii.dev/" className="text-link hover:underline underline-offset-2">
-          https://niskii.dev/
+        <a href="https://selim1337.tk/" className="text-link hover:underline underline-offset-2">
+          https://selim1337.tk
         </a>
       </div>
     </div>
